18857e35e98608d95f678027abb28e389138e197
SunoJoo <32032718+SunoJoo@users.noreply.github.com>
2017-09-21 20:46:21 +0900

    [2017.9.21 #5] Tcp_socker 만듦.

---
+++ CapstoneDesign_dev/src/tcp_sock/Tcp_clinet.java
@@ -0,0 +1,5 @@
+package tcp_sock;
+
+public class Tcp_clinet {
+
+}
---
+++ CapstoneDesign_dev/src/tcp_sock/main.java
@@ -0,0 +1,10 @@
+package tcp_sock;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
